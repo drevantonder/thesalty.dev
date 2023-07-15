1a93ce446ea21156769c055cefb1371cb79719c9
Andre van Tonder <andre@drevan.me>
2023-07-15 16:51:19 +1000

    A humble beginning

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,18 @@
 <template>
-    <div class="bg-gray-950 h-screen w-screen">
-        <h1 class="text-6xl text-orange-400">The Salty Dev</h1>
+    <div class="bg-gray-950 min-h-screen h-screen w-full">
+        <div class="container mx-auto flex flex-col md:flex-row items-center justify-center md:justify-between py-8 gap-8 h-full md:h-auto">
+            <a href="https://blog.thesalty.dev" class="lowercase text-orange-500 font-semibold text-xl tracking-wide">Blog</a>
+            <span title="Coming Soon" class="lowercase text-orange-500 font-semibold text-xl tracking-wide">About</span>
+            <div class="flex flex-col items-center text-orange-400 ">
+                <h1 class="text-4xl md:text-6xl font-display text-center">The Salty Dev</h1>
+                <div class="w-full flex justify-stretch items-center space-x-6 mt-4">
+                    <span class="h-0.5 bg-current w-full rounded-full"></span>
+                    <span class="font-semibold shrink-0 tracking-widest">EST. 2023</span>
+                    <span class="h-0.5 bg-current w-full rounded-full"></span>
+                </div>
+            </div>
+            <a href="https://twitter.com/thesaltydev" rel="noreferrer" class="lowercase text-orange-500 font-semibold text-xl tracking-wide">Twitter</a>
+            <span title="Coming Soon" class="lowercase text-orange-500 font-semibold text-xl tracking-wide">Discord</span>
+        </div>
     </div>
 </template>
-  
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['thirsty-rough-two', defaultTheme.fontFamily.sans],
+        body: ['futura-pt', defaultTheme.fontFamily.sans],
+        sans: ['futura-pt', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
-
