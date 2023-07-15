e99e86ca96f3812742dd0d49a15255e05309c8e5
Andre van Tonder <andre@drevan.me>
2023-07-15 17:23:37 +1000

    Add icons and cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,14 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   app: {
     head: {
-      link: [{ rel: 'stylesheet', href: 'https://use.typekit.net/nmf2npd.css' }]
+      link: [
+        { rel: 'stylesheet', href: 'https://use.typekit.net/nmf2npd.css' },
+        // From https://favicon.io/favicon-converter/
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
     }
   }
 })
